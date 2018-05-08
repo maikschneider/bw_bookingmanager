bcb3827ad2fec661ac6def7ab1ed3561b40f5c17
Maik Schneider <m.schneider@blueways.de>
2018-05-08 18:25:45 +0200

    [TASK] edit date range query to select repeating events only if they occur during range

--- Classes/Domain/Repository/TimeslotRepository.php
+++ Classes/Domain/Repository/TimeslotRepository.php
@@ -36,7 +36,8 @@ class TimeslotRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
                     $query->logicalAnd([
                         $query->contains('calendars', $calendar),
                         $query->greaterThan('repeatType', \Blueways\BwBookingmanager\Domain\Model\Timeslot::REPEAT_NO),
-                        $query->greaterThan('endDate', $startDate->format('Y-m-d H:i:s'))
+                        $query->lessThan('startDate', $endDate->format('Y-m-d 23:59:59')),
+                        $query->greaterThan('endDate', $startDate->format('Y-m-d 00:00:00'))
                     ])
                 ])
             );
