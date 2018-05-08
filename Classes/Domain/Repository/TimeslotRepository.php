20679d1c8b6b36dc215175663ef802dca1967163
Maik Schneider <m.schneider@blueways.de>
2018-05-08 14:08:41 +0200

    [BUGFIX] date range query: show only timeslots attached to calendar

--- Classes/Domain/Repository/TimeslotRepository.php
+++ Classes/Domain/Repository/TimeslotRepository.php
@@ -27,12 +27,14 @@ class TimeslotRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
                 $query->logicalOr([
                     // no repeatable events starting during date range
                     $query->logicalAnd([
+                        $query->contains('calendars', $calendar),
                         $query->equals('repeatType', \Blueways\BwBookingmanager\Domain\Model\Timeslot::REPEAT_NO),
                         $query->greaterThanOrEqual('startDate', $startDate->format('Y-m-d 00:00:00')),
                         $query->lessThanOrEqual('startDate', $endDate->format('Y-m-d 23:59:59')),
                     ]),
                     // repeating events that end during or after date range
                     $query->logicalAnd([
+                        $query->contains('calendars', $calendar),
                         $query->greaterThan('repeatType', \Blueways\BwBookingmanager\Domain\Model\Timeslot::REPEAT_NO),
                         $query->greaterThan('endDate', $startDate->format('Y-m-d H:i:s'))
                     ])
