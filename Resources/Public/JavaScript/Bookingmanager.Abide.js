03c74cd637bbe06a85e8e8cf764c6f11d8635509
Maik Schneider <m.schneider@blueways.de>
2018-06-13 12:50:27 +0200

    [TASK] custom abide validator to check checkbox

--- Resources/Public/JavaScript/Bookingmanager.Abide.js
+++ Resources/Public/JavaScript/Bookingmanager.Abide.js
@@ -10,12 +10,28 @@ function selectboxValidator(
   required, /* is the element required according to the `[required]` attribute */
   parent    /* parent of the jQuery element `$el` */
 ) {
-  return $('option:selected', $el).val() > 0;
+  return $('option:selected', $el).val() !== 0;
+};
+
+/**
+ * Validates checkbox to be checked
+ *
+ * @param {*} $el
+ * @param {*} required
+ * @param {*} parent
+ */
+function checkboxValidator(
+  $el,      /* jQuery element to validate */
+  required, /* is the element required according to the `[required]` attribute */
+  parent    /* parent of the jQuery element `$el` */
+) {
+  return $el.prop('checked')
 };
 
 
 if (Foundation && Foundation.Abide){
 
   Foundation.Abide.defaults.validators['option_selected'] = selectboxValidator;
+  Foundation.Abide.defaults.validators['is_checked'] = checkboxValidator;
 
 }
