8395ee5f4218a3ef7e4b0f3810f57b545604256d
Maik Schneider <m.schenider@blueways.de>
2018-08-27 11:08:40 +0200

    [BUGFIX] do not cache calendar show actiojn

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,7 +13,7 @@ call_user_func(
             ],
             // non-cacheable actions
             [
-                'Calendar' => '',
+                'Calendar' => 'show',
                 'Timeslot' => '',
                 'Entry' => 'create',
             ]
