b03dc4c5b71f53834dbb2cf67544ae6578e3f7be
Maik Schneider <m.schneider@blueways.de>
2019-04-16 11:42:28 +0200

    feat: delete calendar cache on entry/timeslot new/update/delete

--- Classes/Hooks/TCEmainHook.php
+++ Classes/Hooks/TCEmainHook.php
@@ -9,24 +9,48 @@ class TCEmainHook
 
     public function processDatamap_afterAllOperations(\TYPO3\CMS\Core\DataHandling\DataHandler &$pObj)
     {
+        $cache = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class)->getCache('bwbookingmanager_calendar');
+
+        // new / update entry
         if (is_array($pObj->datamap['tx_bwbookingmanager_domain_model_entry'])) {
             foreach ($pObj->datamap['tx_bwbookingmanager_domain_model_entry'] as $entry) {
                 if ($entry['calendar']) {
-                    $cache = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class)->getCache('bwbookingmanager_calendar');
                     $cache->flushByTag('calendar' . $entry['calendar']);
                 }
             }
         }
-    }
 
-    public function processCmdmap_deleteAction(
-        $table,
-        $id,
-        $recordToDelete,
-        $recordWasDeleted = null,
-        \TYPO3\CMS\Core\DataHandling\DataHandler &$pObj
-    ) {
-        $e = $pObj;
+        // new / update timeslot
+        if (is_array($pObj->datamap['tx_bwbookingmanager_domain_model_timeslot'])) {
+            foreach ($pObj->datamap['tx_bwbookingmanager_domain_model_timeslot'] as $timeslot) {
+                if ($timeslot['calendars'] && strlen($timeslot['calendars'])) {
+                    // edit form of tx_bwbookingmanager_domain_model_calendar_1,tx_bwbookingmanager_domain_model_calendar_2
+                    $calendars = explode(',', $timeslot['calendars']);
+                    foreach ($calendars as $calendar) {
+                        $calendarId = substr($calendar, 42);
+                        $cache->flushByTag('calendar' . $calendarId);
+                    }
+                }
+            }
+        }
+
+        // delete Entry
+        if (is_array($pObj->cmdmap['tx_bwbookingmanager_domain_model_entry'])) {
+            foreach ($pObj->cmdmap['tx_bwbookingmanager_domain_model_entry'] as $id => $conf) {
+                if ($conf['delete'] === '1') {
+                    $cache->flushByTag('entry' . $id);
+                }
+            }
+        }
+
+        // delete Timeslot
+        if (is_array($pObj->cmdmap['tx_bwbookingmanager_domain_model_timeslot'])) {
+            foreach ($pObj->cmdmap['tx_bwbookingmanager_domain_model_timeslot'] as $id => $conf) {
+                if ($conf['delete'] === '1') {
+                    $cache->flushByTag('timeslot' . $id);
+                }
+            }
+        }
     }
 
 }
