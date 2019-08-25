5281a70cf94de90f44afe8006477ac69680e02f2
Maik Schneider <m.schneider@blueways.de>
2019-08-25 10:12:54 +0200

    feat: new backend api route for calendar show

--- Classes/Controller/AdministrationController.php
+++ Classes/Controller/AdministrationController.php
@@ -21,8 +21,8 @@ use TYPO3\CMS\Core\FormProtection\FormProtectionFactory;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 use TYPO3\CMS\Core\Utility\HttpUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 use TYPO3\CMS\Extbase\Mvc\View\ViewInterface;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 use TYPO3\CMS\Lang\LanguageService;
@@ -37,7 +37,7 @@ class AdministrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
      * entryRepository
      *
      * @var \Blueways\BwBookingmanager\Domain\Repository\EntryRepository
-     * @inject
+     *
      */
     protected $entryRepository = null;
 
@@ -45,7 +45,7 @@ class AdministrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
      * calendarRepository
      *
      * @var \Blueways\BwBookingmanager\Domain\Repository\CalendarRepository
-     * @inject
+     *
      */
     protected $calendarRepository = null;
 
@@ -82,6 +82,171 @@ class AdministrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
         parent::initializeAction();
     }
 
+    public function indexAction()
+    {
+        $hideForm = true;
+        $demandVars = GeneralUtility::_GET('tx_bwbookingmanager_web_bwbookingmanagertxbookingmanagerm1');
+        $demand = GeneralUtility::makeInstance(AdministrationDemand::class);
+        // override default demand values with values from GET request
+        if (is_array($demandVars['demand'])) {
+            $hideForm = false;
+            foreach ($demandVars['demand'] as $key => $value) {
+                if (property_exists(AdministrationDemand::class, $key)) {
+                    $getter = 'set' . ucfirst($key);
+                    $demand->$getter($value);
+                }
+            }
+        }
+
+        $calendars = $this->calendarRepository->findAll();
+
+        // use first calendar as default
+        if (!$calendars && count($calendars)) {
+            $calendar = $calendars->getFirst();
+        }
+
+        $this->view->assign('hideForm', $hideForm);
+        $this->view->assign('page', $this->pageUid);
+        $this->view->assign('demand', $demand);
+        $this->view->assign('moduleToken', $this->getToken(true));
+        $this->view->assign('calendar', $calendar);
+        $this->view->assign('calendars', $calendars);
+    }
+
+    /**
+     * Get a CSRF token
+     *
+     * @param bool $tokenOnly Set it to TRUE to get only the token, otherwise including the &moduleToken= as prefix
+     * @return string
+     */
+    protected function getToken($tokenOnly = false)
+    {
+        if (self::is9up()) {
+            $tokenParameterName = 'token';
+            $token = FormProtectionFactory::get('backend')->generateToken(
+                'route',
+                'web_BwBookingmanagerTxBookingmanagerM1'
+            );
+        } else {
+            $tokenParameterName = 'moduleToken';
+            $token = FormProtectionFactory::get()->generateToken(
+                'moduleCall',
+                'web_BwBookingmanagerTxBookingmanagerM1'
+            );
+        }
+
+        if ($tokenOnly) {
+            return $token;
+        }
+
+        return '&' . $tokenParameterName . '=' . $token;
+    }
+
+    public function blockslotAction()
+    {
+        $hideForm = true;
+
+        $calendars = $this->calendarRepository->findAll();
+
+        $this->view->assign('hideForm', $hideForm);
+        $this->view->assign('page', $this->pageUid);
+        $this->view->assign('moduleToken', $this->getToken(true));
+        $this->view->assign('calendars', $calendars);
+    }
+
+    public function dashboardAction()
+    {
+        $calendars = $this->calendarRepository->findAllIgnorePid();
+
+        $startDate = new \DateTime('now');
+        $startDate = $startDate->format('d-m-Y');
+
+        $chart1UriYear = \Blueways\BwBookingmanager\Helper\DashboardCharts::getStaticDashboardChartUri(
+            'ajax_dashboard_chart1',
+            ['view' => 'year', 'startDate' => $startDate]
+        );
+        $chart1UriMonth = \Blueways\BwBookingmanager\Helper\DashboardCharts::getStaticDashboardChartUri(
+            'ajax_dashboard_chart1',
+            ['view' => 'month', 'startDate' => $startDate]
+        );
+        $chart1UriWeek = \Blueways\BwBookingmanager\Helper\DashboardCharts::getStaticDashboardChartUri(
+            'ajax_dashboard_chart1',
+            ['view' => 'week', 'startDate' => $startDate]
+        );
+
+        $this->view->assign('calendars', $calendars);
+        $this->view->assign('chart1UriYear', $chart1UriYear);
+        $this->view->assign('chart1UriMonth', $chart1UriMonth);
+        $this->view->assign('chart1UriWeek', $chart1UriWeek);
+    }
+
+    public function timeslotAction()
+    {
+        $calendars = $this->calendarRepository->findAll();
+        $this->view->assign('calendars', $calendars);
+    }
+
+    public function shiftAction()
+    {
+        $calendars = $this->calendarRepository->findAllIgnorePid();
+        $links = [];
+
+        foreach ($calendars as $calendar) {
+            $links[] = \Blueways\BwBookingmanager\Helper\DashboardCharts::getStaticDashboardChartUri(
+                'ajax_api_calendar_show',
+                ['calendar' => $calendar]
+            );
+        }
+
+        $this->view->assign('calendars', $calendars);
+        $this->view->assign('links', $links);
+    }
+
+    /**
+     * Redirect to form to create a new Entry record
+     */
+    public function newEntryAction()
+    {
+        $this->redirectToCreateNewRecord('tx_bwbookingmanager_domain_model_entry');
+    }
+
+    /**
+     * Redirect to tceform creating a new record
+     *
+     * @param string $table table name
+     */
+    private function redirectToCreateNewRecord($table)
+    {
+        $pid = $this->pageUid;
+        if ($pid === 0 && isset($this->tsConfiguration['defaultPid.'])
+            && is_array($this->tsConfiguration['defaultPid.'])
+            && isset($this->tsConfiguration['defaultPid.'][$table])
+        ) {
+            $pid = (int)$this->tsConfiguration['defaultPid.'][$table];
+        }
+
+        if (self::is9up()) {
+            $returnUrl = 'index.php?route=/web/BwBookingmanagerTxBookingmanagerM1/';
+        } else {
+            $returnUrl = 'index.php?M=web_BwBookingmanagerTxBookingmanagerM1';
+        }
+
+        $returnUrl .= '&id=' . $this->pageUid . $this->getToken();
+        $url = BackendUtilityCore::getModuleUrl('record_edit', [
+            'edit[' . $table . '][' . $pid . ']' => 'new',
+            'returnUrl' => $returnUrl
+        ]);
+        HttpUtility::redirect($url);
+    }
+
+    /**
+     * Redirect to form to create a new Blockslot record
+     */
+    public function newBlockslotAction()
+    {
+        $this->redirectToCreateNewRecord('tx_bwbookingmanager_domain_model_blockslot');
+    }
+
     protected function initializeView(\TYPO3\CMS\Extbase\Mvc\View\ViewInterface $view)
     {
         parent::initializeView($view);
@@ -98,6 +263,17 @@ class AdministrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
         $this->createMenu();
         $this->createButtons();
         $view->assign('is9up', self::is9up());
+    }
+
+    public function injectCalendarRepository(
+        \Blueways\BwBookingmanager\Domain\Repository\CalendarRepository $calendarRepository
+    ) {
+        $this->calendarRepository = $calendarRepository;
+    }
+
+    public function injectEntryRepository(\Blueways\BwBookingmanager\Domain\Repository\EntryRepository $entryRepository)
+    {
+        $this->entryRepository = $entryRepository;
     }
 
     /**
@@ -127,6 +303,31 @@ class AdministrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
         if (is_array($this->pageInformation)) {
             $this->view->getModuleTemplate()->getDocHeaderComponent()->setMetaInformation($this->pageInformation);
         }
+    }
+
+    /**
+     * Returns the LanguageService
+     *
+     * @return LanguageService
+     */
+    protected function getLanguageService()
+    {
+        return $GLOBALS['LANG'];
+    }
+
+    /**
+     * Creates the URI for a backend action
+     *
+     * @param string $controller
+     * @param string $action
+     * @param array $parameters
+     * @return string
+     */
+    protected function getHref($controller, $action, $parameters = [])
+    {
+        $uriBuilder = $this->objectManager->get(UriBuilder::class);
+        $uriBuilder->setRequest($this->request);
+        return $uriBuilder->reset()->uriFor($action, $parameters, $controller);
     }
 
     /**
@@ -180,23 +381,31 @@ class AdministrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
             ]
         ];
         foreach ($buttons as $key => $tableConfiguration) {
-            if ($this->getBackendUser()->isAdmin() || GeneralUtility::inList($this->getBackendUser()->groupData['tables_modify'],
-                    $tableConfiguration['table'])
+            if ($this->getBackendUser()->isAdmin() || GeneralUtility::inList(
+                $this->getBackendUser()->groupData['tables_modify'],
+                $tableConfiguration['table']
+            )
             ) {
                 // @TODO repair translation
                 //$title = $this->getLanguageService()->sL('LLL:EXT:bw_bookingmanager/Resources/Private/Language/locallang_be.xlf:' . $tableConfiguration['label']);
                 $title = $tableConfiguration['label'];
                 $viewButton = $buttonBar->makeLinkButton()
-                    ->setHref($uriBuilder->reset()->setRequest($this->request)->uriFor($tableConfiguration['action'],
-                        [], 'Administration'))
+                    ->setHref($uriBuilder->reset()->setRequest($this->request)->uriFor(
+                        $tableConfiguration['action'],
+                        [],
+                        'Administration'
+                    ))
                     ->setDataAttributes([
                         'toggle' => 'tooltip',
                         'placement' => 'bottom',
                         'title' => $title
                     ])
                     ->setTitle($title)
-                    ->setIcon($this->iconFactory->getIcon($tableConfiguration['icon'], Icon::SIZE_SMALL,
-                        'overlay-new'));
+                    ->setIcon($this->iconFactory->getIcon(
+                        $tableConfiguration['icon'],
+                        Icon::SIZE_SMALL,
+                        'overlay-new'
+                    ));
                 $buttonBar->addButton($viewButton, ButtonBar::BUTTON_POSITION_LEFT, 2);
             }
         }
@@ -211,123 +420,6 @@ class AdministrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
     }
 
     /**
-     * Creates the URI for a backend action
-     *
-     * @param string $controller
-     * @param string $action
-     * @param array $parameters
-     * @return string
-     */
-    protected function getHref($controller, $action, $parameters = [])
-    {
-        $uriBuilder = $this->objectManager->get(UriBuilder::class);
-        $uriBuilder->setRequest($this->request);
-        return $uriBuilder->reset()->uriFor($action, $parameters, $controller);
-    }
-
-    public function indexAction()
-    {
-        $hideForm = true;
-        $demandVars = GeneralUtility::_GET('tx_bwbookingmanager_web_bwbookingmanagertxbookingmanagerm1');
-        $demand = GeneralUtility::makeInstance(AdministrationDemand::class);
-        // override default demand values with values from GET request
-        if (is_array($demandVars['demand'])) {
-            $hideForm = false;
-            foreach ($demandVars['demand'] as $key => $value) {
-                if (property_exists(AdministrationDemand::class, $key)) {
-                    $getter = 'set' . ucfirst($key);
-                    $demand->$getter($value);
-                }
-            }
-        }
-
-        $calendars = $this->calendarRepository->findAll();
-
-        // use first calendar as default
-        if (!$calendars && count($calendars)) {
-            $calendar = $calendars->getFirst();
-        }
-
-        $this->view->assign('hideForm', $hideForm);
-        $this->view->assign('page', $this->pageUid);
-        $this->view->assign('demand', $demand);
-        $this->view->assign('moduleToken', $this->getToken(true));
-        $this->view->assign('calendar', $calendar);
-        $this->view->assign('calendars', $calendars);
-    }
-
-    public function blockslotAction()
-    {
-        $hideForm = true;
-
-        $calendars = $this->calendarRepository->findAll();
-
-        $this->view->assign('hideForm', $hideForm);
-        $this->view->assign('page', $this->pageUid);
-        $this->view->assign('moduleToken', $this->getToken(true));
-        $this->view->assign('calendars', $calendars);
-    }
-
-    public function dashboardAction()
-    {
-        $calendars = $this->calendarRepository->findAllIgnorePid();
-
-        $startDate = new \DateTime('now');
-        $startDate = $startDate->format('d-m-Y');
-
-        $chart1UriYear = \Blueways\BwBookingmanager\Helper\DashboardCharts::getStaticDashboardChartUri('ajax_dashboard_chart1',
-            ['view' => 'year', 'startDate' => $startDate]);
-        $chart1UriMonth = \Blueways\BwBookingmanager\Helper\DashboardCharts::getStaticDashboardChartUri('ajax_dashboard_chart1',
-            ['view' => 'month', 'startDate' => $startDate]);
-        $chart1UriWeek = \Blueways\BwBookingmanager\Helper\DashboardCharts::getStaticDashboardChartUri('ajax_dashboard_chart1',
-            ['view' => 'week', 'startDate' => $startDate]);
-
-        $this->view->assign('calendars', $calendars);
-        $this->view->assign('chart1UriYear', $chart1UriYear);
-        $this->view->assign('chart1UriMonth', $chart1UriMonth);
-        $this->view->assign('chart1UriWeek', $chart1UriWeek);
-    }
-
-    /**
-     * Get a CSRF token
-     *
-     * @param bool $tokenOnly Set it to TRUE to get only the token, otherwise including the &moduleToken= as prefix
-     * @return string
-     */
-    protected function getToken($tokenOnly = false)
-    {
-        if (self::is9up()) {
-            $tokenParameterName = 'token';
-            $token = FormProtectionFactory::get('backend')->generateToken('route', 'web_BwBookingmanagerTxBookingmanagerM1');
-        } else {
-            $tokenParameterName = 'moduleToken';
-            $token = FormProtectionFactory::get()->generateToken('moduleCall', 'web_BwBookingmanagerTxBookingmanagerM1');
-        }
-
-        if ($tokenOnly) {
-            return $token;
-        }
-
-        return '&' . $tokenParameterName . '=' . $token;
-    }
-
-    public function timeslotAction()
-    {
-        $calendars = $this->calendarRepository->findAll();
-        $this->view->assign('calendars', $calendars);
-    }
-
-    /**
-     * Returns the LanguageService
-     *
-     * @return LanguageService
-     */
-    protected function getLanguageService()
-    {
-        return $GLOBALS['LANG'];
-    }
-
-    /**
      * Get backend user
      *
      * @return \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
@@ -338,63 +430,10 @@ class AdministrationController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionC
     }
 
     /**
-     * Redirect to tceform creating a new record
-     *
-     * @param string $table table name
-     */
-    private function redirectToCreateNewRecord($table)
-    {
-        $pid = $this->pageUid;
-        if ($pid === 0 && isset($this->tsConfiguration['defaultPid.'])
-            && is_array($this->tsConfiguration['defaultPid.'])
-            && isset($this->tsConfiguration['defaultPid.'][$table])
-        ) {
-            $pid = (int)$this->tsConfiguration['defaultPid.'][$table];
-        }
-
-        if (self::is9up()) {
-            $returnUrl = 'index.php?route=/web/BwBookingmanagerTxBookingmanagerM1/';
-        } else {
-            $returnUrl = 'index.php?M=web_BwBookingmanagerTxBookingmanagerM1';
-        }
-
-        $returnUrl .= '&id=' . $this->pageUid . $this->getToken();
-        $url = BackendUtilityCore::getModuleUrl('record_edit', [
-            'edit[' . $table . '][' . $pid . ']' => 'new',
-            'returnUrl' => $returnUrl
-        ]);
-        HttpUtility::redirect($url);
-    }
-
-    public function shiftAction()
-    {
-        $calendars = $this->calendarRepository->findAllIgnorePid();
-
-        $this->view->assign('calendars', $calendars);
-    }
-
-    /**
-     * Redirect to form to create a new Entry record
-     */
-    public function newEntryAction()
-    {
-        $this->redirectToCreateNewRecord('tx_bwbookingmanager_domain_model_entry');
-    }
-
-    /**
-     * Redirect to form to create a new Blockslot record
-     */
-    public function newBlockslotAction()
-    {
-        $this->redirectToCreateNewRecord('tx_bwbookingmanager_domain_model_blockslot');
-    }
-
-    /**
      * @return bool
      */
     private static function is9up(): bool
     {
         return VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 9000000;
     }
-
 }
