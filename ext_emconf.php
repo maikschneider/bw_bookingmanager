f81029b2373068f7c61cd42b78ccb11f02407667
Maik Schneider <m.schneider@blueways.de>
2020-12-11 09:47:01 +0100

    chore: raise version, update composer

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '7.0.4',
+    'version' => '8.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.20-9.9.99',
+            'bw_email' => '2.1.4-2.9.99'
         ],
         'conflicts' => [],
         'suggests' => [],
