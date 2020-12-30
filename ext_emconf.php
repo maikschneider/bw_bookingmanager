f5abb363929e7fb14f833d630012da9aa19ecbd0
Maik Schneider <m.schneider@blueways.de>
2020-12-30 13:30:13 +0100

    chore: set required typo3 version to 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '8.0.0',
+    'version' => '9.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.20-9.9.99',
+            'typo3' => '9.5.0-9.9.99',
             'bw_email' => '2.1.4-2.9.99'
         ],
         'conflicts' => [],
