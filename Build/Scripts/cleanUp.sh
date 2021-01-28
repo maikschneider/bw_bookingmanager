c22eb81ab4be837e59f6dd54ee233b1f87a37ec8
Maik Schneider <m.schneider@blueways.de>
2021-01-28 11:20:29 +0100

    fix: add @ts-nocheck to Modal.ts of t3 core, remove files in cleanup script

--- Build/Scripts/cleanUp.sh
+++ Build/Scripts/cleanUp.sh
@@ -1,1 +1,11 @@
 echo "Cleaning up.."
+rm -rf Documentation
+rm -rf Tests
+rm .*
+rm bitbucket-pipelines.yml
+rm composer.lock
+rm package.json
+rm tsconfig.json
+rm webpack.*.js
+rm -rf vendor
+rm -rf public
