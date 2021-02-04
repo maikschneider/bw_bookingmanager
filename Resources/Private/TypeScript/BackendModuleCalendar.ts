817ea21a5eb78dd4f4f2b95faf0b94026d2a5123
Maik Schneider <m.schneider@blueways.de>
2021-02-04 23:19:55 +0100

    feat: change build script

--- Resources/Private/TypeScript/BackendModuleCalendar.ts
+++ Resources/Private/TypeScript/BackendModuleCalendar.ts
@@ -31,6 +31,7 @@ class BackendModuleCalendar {
   public viewState: BackendCalendarViewState;
 
   public init() {
+    console.log('hello world!');
     this.initCalendar();
     this.bindEvents();
     return this;
