7e123afe7a7a06d4f559faaadb190785b830411e
Maik Schneider <m.schneider@blueways.de>
2022-04-11 11:56:46 +0200

    chore: remove old chart backend routes

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -1,16 +1,8 @@
 <?php
 
-use Blueways\BwBookingmanager\Controller\Ajax\ChartsController;
 use Blueways\BwBookingmanager\Controller\Ajax\ApiController;
+
 return [
-    'dashboard_chart1' => [
-        'path' => '/dashboard/chart1',
-        'target' => ChartsController::class . '::getChart1'
-    ],
-    'dashboard_chart2' => [
-        'path' => '/dashboard/chart2',
-        'target' => ChartsController::class . '::getChart2'
-    ],
     'api_calendar_show' => [
         'path' => '/api/calendar/show',
         'target' => ApiController::class . '::calendarShowAction'
