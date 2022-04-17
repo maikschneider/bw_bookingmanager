dcd4081eec8749145fd07a0b325a0183d0149de3
Maik Schneider <m.schneider@blueways.de>
2022-04-17 12:27:31 +0200

    feat: refactor ics controller

--- Classes/Controller/IcsController.php
+++ Classes/Controller/IcsController.php
@@ -6,24 +6,23 @@ use Blueways\BwBookingmanager\Domain\Model\Ics;
 use Blueways\BwBookingmanager\Utility\IcsUtility;
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
-use TYPO3\CMS\Extbase\Mvc\Exception\StopActionException;
 
 class IcsController extends ActionController
 {
-    /**
-     * @param Ics $ics
-     * @param string $secret
-     * @return string
-     * @throws StopActionException
-     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\UnsupportedRequestTypeException
-     */
+
+    protected IcsUtility $icsUtility;
+
+    public function __construct(IcsUtility $icsUtility)
+    {
+        $this->icsUtility = $icsUtility;
+    }
+    
     public function showAction(Ics $ics, string $secret = ''): ResponseInterface
     {
         if ($ics->getSecret() !== $secret) {
             $this->throwStatus(403, 'Incorrect secret');
         }
 
-        $icsUtil = $this->objectManager->get(IcsUtility::class);
-        return $this->htmlResponse($icsUtil->getFromIcs($ics));
+        return $this->htmlResponse($this->icsUtility->getFromIcs($ics));
     }
 }
